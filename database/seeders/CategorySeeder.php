8778953205d3b2c0f9fcfc988dd18101457c0ba6
unknown <bouranjaothmane@gmail.com>
2022-04-30 18:42:15 +0000

    insert data to basse donne using seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -13,6 +15,27 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        Category::create(
+            [
+                'name' => 'Web Desing',
+                'slug' => Str::slug('Web Desing')
+            ],
+            [
+                'name' => 'Web Dev',
+                'slug' => Str::slug('Web Dev')
+            ],
+            [
+                'name' => 'Programming',
+                'slug' => Str::slug('Programming')
+            ],
+            [
+                'name' => 'Mobille Apps',
+                'slug' => Str::slug('Mobille Apps')
+            ],
+            [
+                'name' => 'Flliter',
+                'slug' => Str::slug('Flliter')
+            ],
+        );
     }
 }
