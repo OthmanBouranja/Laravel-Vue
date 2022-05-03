50ccb885a7bc8ee642d51ef0b4633871c3ffaf71
unknown <bouranjaothmane@gmail.com>
2022-05-03 13:33:40 +0000

    register & login 1

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -39,11 +39,27 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
 
-            return Limit::perMinute(5)->by($email.$request->ip());
+            return Limit::perMinute(5)->by($email . $request->ip());
         });
 
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+
+        Fortify::loginView(function () {
+            return view("auth.login");
+        });
+        Fortify::registerView(function () {
+            return view("auth.register");
+        });
+        Fortify::verifyEmailView(function () {
+            return view("auth.verify");
+        });
+        Fortify::requestPasswordResetLinkView(function () {
+            return view("auth.reset-password-link");
+        });
+        Fortify::resetPasswordView(function () {
+            return view("auth.reset-password");
         });
     }
 }
