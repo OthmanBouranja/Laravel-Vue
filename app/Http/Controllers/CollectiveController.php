329be4229c1d35cf77c09aab3cde1c5529fbc8cb
unknown <bouranjaothmane@gmail.com>
2022-05-07 19:57:31 +0000

    create les controlles

---
+++ app/Http/Controllers/CollectiveController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Collective;
+use Illuminate\Http\Request;
+
+class CollectiveController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $collectives = Collective::where('user_id', auth()->id)->latest();
+        return view('collectives.index', compact('collectives'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Collective  $collective
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Collective $collective)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Collective  $collective
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Collective $collective)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Collective  $collective
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Collective $collective)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Collective  $collective
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Collective $collective)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth', 'verified')->get('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
